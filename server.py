390bfeddfa683e15519d570fb0ca06fa78eaf8f2
chanryan0528 <93378079+chanryan0528@users.noreply.github.com>
2021-11-01 12:22:29 +0800

    Update server.py

--- server.py
+++ server.py
@@ -2,11 +2,17 @@ from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
 import os
-import json
 import urllib.request
 
-def hello_world(request):
+def stock_price(request):
     url = "http://hq.sinajs.cn/list=sh000001"
+    if name == None or len(name) == 0:
+        name = "world"
+    message = "Hello, " + name + "!\n"
+    return Response(message)
+
+def hello_world(request):
+    name = "Ryan"
     if name == None or len(name) == 0:
         name = "world"
     message = "Hello, " + name + "!\n"
@@ -15,7 +21,9 @@ def hello_world(request):
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
     with Configurator() as config:
-        config.add_route('hello', '/')
+        config.add_route('stock', '/')
+        config.add_route('hello', '/hello')
+        config.add_view(stock_price, route_name='stock')
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', port, app)
